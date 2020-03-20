cd4cbbf741f8fea063bf83c84d42277ba353a430
RobinHankin <hankin.robin@gmail.com>
2020-03-21 12:15:34 +1300

    tweaks

--- rainbow.R
+++ rainbow.R
@@ -5,21 +5,22 @@
 source("usefulrainbowfuncs.R")
 
 n <- 4/3 # refractive index
-
+small <- 1e-9  # nominal small value for numerical stability
 
 plot(NA,xlab='',ylab='',asp=1,xlim=c(0.2,0.3),ylim=c(-1,-0.9))
-#plot(NA,xlab='',ylab='',asp=1,xlim=c(-4,1),ylim=c(-3,1))
+plot(NA,xlab='',ylab='',asp=1,xlim=c(-4,0),ylim=c(-3,1))
 
 ## First plot the bounding circle:
 size <- 2.0
 a <- seq(from=0,to=2*pi,len=1000)  # 'a' for angle
 points(sin(a),cos(a),asp=1,type='l')
 
-small <- 1e-9
+
 #for(a in seq(from=0.52,to=sin(2*asin(1/n))-small,by=0.0005)){
 for(a in seq(from=0.52,to=1-small,by=0.005)){
   drawray(a,lwd=0.2)
 }
+
 
 drawray(atan(1/n),col='red',lwd=1)
 drawray(1-small,col='blue')
@@ -33,8 +34,3 @@ segments(
     col='blue')
              
 
-## Axial line is indeterminate, has to be plotted explicitly:
-abline(h=0)
-
-## Mask outside of circle:
-#mask(1.001)
