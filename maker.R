386c614354192761a71354aa9a8ae59ce9539953
RobinHankin <hankin.robin@gmail.com>
2020-03-23 10:31:43 +1300

    consistentify the filenames

--- maker.R
+++ maker.R
@@ -18,11 +18,7 @@ small <- 1e-9  # nominal small value for numerical stability
 descartes(xlim=c(0.1,0.4),ylim=c(-1.1,-0.9),lwd=0.1,rays=seq(from=0.82,to=1-small,by=0.001))
 dev.off()
 
-pdf(file="plot4.pdf",height=9, width=9)
-descartes(xlim=c(0.3,0.35),ylim=c(-1.1,-0.9),lwd=0.1,rays=seq(from=0.82,to=1-small,by=0.001),doreflect=FALSE)
-dev.off()
-
-pdf(file="plot4a.pdf",height=9, width=9)
+pdf(file="descartes4.pdf",height=9, width=9)
 descartes(xlim=c(0.5,1),ylim=c(0,0.5),lwd=0.1,dolegend=FALSE)
 legend("topright",pch=NA,lty=1,
        col=c("red","green","blue"),
@@ -30,18 +26,18 @@ legend("topright",pch=NA,lty=1,
        )
 dev.off()
 
-pdf(file="plot5.pdf",height=9, width=9)
+pdf(file="fraunhofer1.pdf",height=9, width=9)
 fraunhofer()
 dev.off()
 
-pdf(file="plot6.pdf",height=9, width=9)
+pdf(file="fraunhofer2.pdf",height=9, width=9)
 fraunhofer(xlim=c(-0.4,0.4),ylim=c(-1,-0.7), bvals = seq(from=5,to=15,len=400), dvals =  seq(from = 0.4, to = 1, len = 100) )
 dev.off()
 
-pdf(file="plot7.pdf",height=9, width=9)
+pdf(file="fraunhofer3.pdf",height=9, width=9)
 fraunhofer(xlim=c(0.2,0.4),ylim=c(-1,-0.9), bvals = seq(from=6.3,to=6.8,len=100),dvals = seq(from = 0.4, to = 1, len = 9000))
 dev.off()
 
-pdf(file="plot8.pdf",height=9, width=9)
+pdf(file="fraunhofer4.pdf",height=9, width=9)
 fraunhofer(xlim=c(0.2,0.3),ylim=c(-1,-0.94), bvals = seq(from=6.4,to=6.6,len=100),dvals = seq(from = 0.4, to = 1, len = 9000))
 dev.off()
