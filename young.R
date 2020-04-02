3558eb07d9136358a898317aec968d0caaa8a935
RobinHankin <hankin.robin@gmail.com>
2020-04-03 08:40:56 +1300

    further work

--- young.R
+++ young.R
@@ -17,19 +17,20 @@ options("refractive_index" = 1.333)  # consistent with Laven 2017
 
     ## Further observe that if n=4/3 we get f(b0) = 139.97
 
-
+    ## NB theta in radians
     
     f <- function(b){scattering_angle(b,n=n)-theta}
+
     c(
-        uniroot(f,c(0,b0))$root,
-        uniroot(f,c(b0,1))$root
-    )
+        uniroot(f,c(0,b0),tol=1e-13)$root,  # red ray in Laven's Fig 3
+        uniroot(f,c(b0,1),tol=1e-13)$root   # blue ray in Laven's Fig 3
+    )                                       # (cyan line is the Cartesian ray)
 }
 
 `drawfig3` <- function(theta=141,n=getOption("refractive_index")){
     ## (could use descartes() here but we need more control)
     jj <- 1.3
-    plot(NA,xlim=c(-jj,jj),ylim=c(-jj,jj),asp=1,axes=FALSE,xlab="",ylab="")
+    plot(NA,xlim=c(-jj,jj),ylim=c(-jj,jj),asp=1,axes=TRUE,xlab="",ylab="")
     drawdrop()
     cartesian_ray(col="cyan")
     jj <- fig3(theta)
@@ -41,7 +42,7 @@ options("refractive_index" = 1.333)  # consistent with Laven 2017
     x0 <- cos(thetarad)
     y0 <- -sin(thetarad)
     l <- 5
-    segments(-1,0,-1,1,lty=2,col="gray")  # start point 
+    segments(-1, 0,-1     ,1      ,lty=2,col="gray") # start point 
     segments(x0,y0,x0-y0*l,y0+x0*l,lty=2,col="gray") # end point
 
     ## rays are assumed to be in phase at 
@@ -56,5 +57,68 @@ options("refractive_index" = 1.333)  # consistent with Laven 2017
         seq(from=0.93,to=1,len=100)),
         type="l",col="yellow")
 }
+
+
+
+## Function raypathlength_single() defined below is a sort of inverse
+## to raydist().  Given impact parameter d, it returns the optical
+## path length.  If light passes from air to glass, the frequency
+## stays the same but the speed changes from c to c/n, so the
+## wavelength changes from from lambda to lambda/n.  The path starts
+## from (-1,d) and ends at the intersection of the ray and the second
+## dotted line shown in drawfig3().
+##
+## NB Function raypathlength_single() does NOT account for the Gouy
+## phase shift.
+
+`raypathlength_single` <- function(d,n=getOption("refractive_index")){  
+    M <- raytracer(d)   # get 'd' from fig3()
+
+    ## First figure out where the exiting ray intersects the end line:
+     
+    theta <- pi-M[3,3]  # something close to 140 degrees or 2.443 rad
+
+    ## equation for *normal* to outgoing light is (y-y0)/(x-x0) = g0:
+    x0 <-  cos(theta)
+    y0 <- -sin(theta)
+    g0 <-  cot(theta)   # e.g.    abline(y0-g0*x0,g0,col='green')
+    ## equation for outgoing light ray is (y-y1)/(x-x1) = g1:
+
+    x1 <- M[3,1]
+    y1 <- M[3,2]
+    g1 <- tan(M[3,3])  # e.g.    abline(y1-g1*x1,g1,col='green')
+
+    ## intersection point:
+    x <- ((y0-y1) +g1*x1 -g0*x0)/(g1-g0)
+    y <- y0 +g0*x -g0*x0
+    l <- (
+        + (1+M[1,1])                      *1  # initial ray,         in vacuo
+        + sqrt(sum((M[1,1:2]-M[2,1:2])^2))*n  # first internal leg,  in water
+        + sqrt(sum((M[2,1:2]-M[3,1:2])^2))*n  # second internal leg, in water
+        + sqrt(sum((M[3,1:2]-c(x,y)  )^2))*1  # exit ray,            in vacuo
+    )
+    return(l)
+}
+
+## In function phase_diff(), theta is the exit angle in DEGREES;
+## default theta=141.  lambda is the wavelength of the light divided
+## by the radius of the drop.  Red light has lambda of 0.65
+## micrometers and the drop has a notional radius of 100 um which
+## gives lambda = 0.65/100 = 0.0065.
+
+`phase_diff` <- function(theta=141,lambda=0.0065){
+
+    jj <- fig3(theta)  # jj[1] is the red ray, jj[2] is the blue ray
+
+    path1 <- (raypathlength_single(jj[1]) %% lambda) 
+    ## Laven's Figure 7: phase advanced by pi/2 at A and B
+
+    path2 <- (raypathlength_single(jj[2]) %% lambda) 
+    ## Laven's Figure 8: phase advanced by pi/2 at A, B, and C
+
+    delta <- (path1-path2)*2*pi
+    return(1+sin(2*delta))
+}
+
 
 
