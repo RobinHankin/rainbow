e765347f238e6045d959ff72bef5212672bdd8cf
Robin Hankin <hankin.robin@gmail.com>
2022-07-07 10:39:31 +1200

    mistake

--- coffee_cup_caustic.R
+++ coffee_cup_caustic.R
@@ -1,1 +1,1 @@
-stop("this does not work: to produce a coffee cup caustic, source caustic.R which is in the *string* repo")
+stop("this does not work: to produce a coffee cup caustic, source caustic2.R which is in the *string* repo")
